d9cb8dad6470ab1e49c9dc4c2a057fd6837093bf
rmnnsk <roman08oskin@gmail.com>
2020-11-17 23:00:51 +0300

    hw6

---
+++ 06/my_format.h
@@ -0,0 +1,118 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <sstream>
+#include <cassert>
+
+struct ReplaceInfo
+{
+    int first_ind;
+    int last_ind;
+    int arg_number;
+
+    ReplaceInfo() = default;
+
+    ReplaceInfo(int fi, int li, int arg) : first_ind(fi), last_ind(li), arg_number(arg)
+    {};
+};
+
+
+class LowArgs_Exception : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Not enough arguments to format.";
+    }
+};
+
+class WrongFormat_Exception : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Incorrect format string.";
+    }
+};
+
+class WrongArg_Exception : public std::exception
+{
+public:
+    const char* what() const noexcept override
+    {
+        return "Argument cannot be used with output streams.";
+    }
+};
+
+std::vector<std::string> process()
+{
+    return std::vector<std::string>();
+}
+
+template<class T, class ...Args>
+std::vector<std::string> process(T cur, Args ...other)
+{
+    std::vector<std::string> res;
+    std::stringstream casting_stream;
+    casting_stream << cur;
+    res.push_back(casting_stream.str());
+    auto proc_next = process(other...);
+    res.insert(res.end(), proc_next.begin(), proc_next.end());
+    return res;
+}
+
+template<class ...Args>
+std::vector<std::string> get_args_strings(Args ...args)
+{
+    return process(args...);
+}
+
+template<class ...Args>
+std::string format(const std::string& fmt, Args ...args)
+{
+    std::string tmp = fmt;
+    int cur_ind = 0;
+    bool inside_param = false;
+    int max_param = -1;
+    int last_open = -1;
+    std::vector<ReplaceInfo> to_replace;
+    while (cur_ind < fmt.size()) {
+
+        if (fmt[cur_ind] == '{') {
+            if (inside_param) {
+                throw WrongFormat_Exception();
+            }
+            last_open = cur_ind;
+            inside_param = true;
+        } else if (fmt[cur_ind] == '}') {
+            if (!inside_param || last_open == cur_ind - 1) {
+                throw WrongFormat_Exception();
+            }
+            std::string arg_string = fmt.substr(last_open + 1, cur_ind - last_open - 1);
+            int arg_ind = std::stoi(arg_string);
+            to_replace.emplace_back(last_open, cur_ind, arg_ind);
+            max_param = std::max(max_param, arg_ind);
+            inside_param = false;
+        } else if (inside_param) {
+            auto sym = static_cast<unsigned char>(fmt[cur_ind]);
+            if (!isdigit(sym)) {
+                throw WrongFormat_Exception();
+            }
+        }
+        cur_ind++;
+    }
+    if (max_param >= sizeof...(args)) {
+        throw LowArgs_Exception();
+    }
+    auto args_strings = get_args_strings(args...);
+    int delta = 0;
+    for (const auto& info : to_replace) {
+        tmp.replace(info.first_ind - delta, info.last_ind - info.first_ind + 1, args_strings[info.arg_number]);
+        delta += (info.last_ind - info.first_ind + 1) - (int)args_strings[info.arg_number].size();
+    }
+
+    return tmp;
+}
---
+++ 06/tests.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <functional>
+#include <vector>
+
+#include "my_format.h"
+
+void test1()
+{
+    std::cout << "TEST 1" << std::endl;
+    std::cout << "TEST FROM TASK DESCRIPTION." << std::endl;
+    std::cout << R"(format("{1}+{1} = {0}", 2, "one"))" << std::endl;
+    auto text = format("{1}+{1} = {0}", 2, "one");
+    assert(text == "one+one = 2");
+}
+
+void test2()
+{
+    std::cout << "TEST 2" << std::endl;
+    std::cout << "TESTING WRONG FORMAT STRINGS." << std::endl;
+    std::cout << R"(format("{-1}+{1} = {0}", 2, "one"))" << std::endl;
+    try {
+        auto text = format("{-1}+{1} = {0}", 2, "one");
+    }
+    catch (WrongFormat_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+    std::cout << R"(format("{first}+{1} = {0}", 2, "one"))" << std::endl;
+    try {
+        auto text = format("{first}+{1} = {0}", 2, "one");
+    }
+    catch (WrongFormat_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+    std::cout << R"(format("{}+{1} = {0}", 2, "one"))" << std::endl;
+    try {
+        auto text = format("{}+{1} = {0}", 2, "one");
+    }
+    catch (WrongFormat_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+    std::cout << R"(format("{1 + {1} = {0}", 2, "one"))" << std::endl;
+    try {
+        auto text = format("{ + {1} = {0}", 2, "one");
+    }
+    catch (WrongFormat_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+    std::cout << R"(format("test}", 2, "one"))" << std::endl;
+    try {
+        auto text = format("test}", 2, "one");
+    }
+    catch (WrongFormat_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+}
+
+void test3()
+{
+    std::cout << "TEST 3" << std::endl;
+    std::cout << "TESTING WRONG ARGS EXCEPTION ." << std::endl;
+    std::cout << R"(format("{1}+{1} = {0}", 2, "one", "test"))" << std::endl;
+    try {
+        auto text = format("{1}+{1} = {0}", 2, "one", "test");
+    }
+    catch (LowArgs_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+    std::cout << R"(format("{1}+{1} = {0}", 2))" << std::endl;
+    try {
+        auto text = format("{1}+{1} = {0}", 2);
+    }
+    catch (LowArgs_Exception& err) {
+        std::cout << "Catch WrongFormat_Exception" << std::endl;
+    }
+}
+
+void test4()
+{
+    std::cout << "TEST 4" << std::endl;
+    std::cout << "ANOTHER WORK EXAMPLE" << std::endl;
+    std::cout << R"(format("{001}+{1} = {0}", 2, "one"))" << std::endl;
+    auto text = format("{001}+{1} = {0}", 2, "one");
+    assert(text == "one+one = 2");
+    std::cout << R"(format("{000}+{00000} = {1}", 2, 5000))" << std::endl;
+    text = format("{000}+{00000} = {1}", 2, 5000);
+    assert(text == "2+2 = 5000");
+    std::cout << R"(format("{0}+{1} != {2}", 1234567, 7654321, "mandarinka"))" << std::endl;
+    text = format("{0}+{1} != {2}", 1234567, 7654321, "mandarinka");
+    assert(text == "1234567+7654321 != mandarinka");
+}
+
+int main()
+{
+    std::vector<std::function<void()>> tests = {test1, test2, test3, test4};
+    for (const auto& test : tests) {
+        test();
+        std::cout << std::endl;
+    }
+}
